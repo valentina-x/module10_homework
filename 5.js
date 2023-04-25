91b9eed10d27fa5de09c347ce93356e56069a743
Valentina <xzxz100.95@mail.ru>
2023-04-25 22:19:34 +0200

    change 5,7

--- 5.js
+++ 5.js
@@ -4,6 +4,6 @@
 let arr = ['JS', false, 345];
 console.log(arr.length);
 
-let result = arr.map(function(item, index, array) {
+arr.forEach(function(item, index, array){
   console.log(item);
-});
+})
